17c46b56b14bba86fd979db10a8633f7bab8ac09
Vaha Dzhauth <vaha_dg@mail.ru>
2020-07-22 17:50:26 +0300

    add all files

---
+++ wxPython/wintest.py
@@ -0,0 +1,14 @@
+import wx
+
+app = wx.App()
+
+frame = wx.Frame(None, title = "Assalamu alaykum!")
+frame.Show()
+
+app.MainLoop()
+
+import wx
+app = wx.App()
+frame = wx.Frame(None, title = "Assamu alaykum!")
+frame.Show()
+app.MainLoop()
